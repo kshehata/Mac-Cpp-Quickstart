0841cf71b55094c37d82121d4b7bf3c49b78588e
Kareem Shehata <kareem@shehata.ca>
2020-02-18 17:51:03 +1100

    Adding usage of utils to example main

--- main.cc
+++ main.cc
@@ -1,6 +1,32 @@
+#include "utils.h"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using std::cout;
+using std::endl;
+
+void usage(const char* name) {
+  cout << "Usage: " << name << " [b1] [b2]" << endl;
+  cout << "b1 and b2 are numbers in hex (base 16) that must be the same length"
+   << endl;
+  cout << "Output is the xor of the two numbers" << endl;
+}
+
+int main(int argc, const char * argv[]) {
+  if (argc != 3) {
+    usage(argv[0]);
+    return 1;
+  }
+
+  auto b1 = hex2bytes(argv[1]);
+  auto b2 = hex2bytes(argv[2]);
+
+  if (b1.size() != b2.size()) {
+    usage(argv[0]);
+    return 2;
+  }
+
+  xor_blocks(b1, b2, b1);
+
+  cout << bytes2hex(b1) << endl;
+  return 0;
 }
